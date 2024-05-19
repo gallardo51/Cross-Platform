739dafc6a08cb2452f9b33f9174d986714e55421
Alex Sobolev <31108@mail.ru>
2024-05-19 17:39:43 +0300

    Initial Commit

---
+++ Cross-Platform/Cross_PlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cross_PlatformApp.swift
+//  Cross-Platform
+//
+//  Created by Александр Соболев on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Cross_PlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
