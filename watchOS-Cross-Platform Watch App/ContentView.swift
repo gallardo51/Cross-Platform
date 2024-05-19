122aec796c8848df398407e5d7edf3c4775b1303
Alex Sobolev <31108@mail.ru>
2024-05-19 20:41:17 +0300

    Setup app for watchOS

---
+++ watchOS-Cross-Platform Watch App/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  watchOS-Cross-Platform Watch App
+//
+//  Created by Александр Соболев on 19.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+       TabBarView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchOS-Cross-Platform Watch App/watchOS_Cross_PlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOS_Cross_PlatformApp.swift
+//  watchOS-Cross-Platform Watch App
+//
+//  Created by Александр Соболев on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_Cross_Platform_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
